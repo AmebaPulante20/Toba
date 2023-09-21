968108e1e8e5872f929054428ce9bddd71e9b842
AmebaPulante20 <143095752+AmebaPulante20@users.noreply.github.com>
2023-09-21 07:56:56 -0300

    dnhfh

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ function Toca () {
 }
 const lista = document.querySelectorAll('.tecla');
 
-let Contador = 0;
+let contador = 0;
+
+const classe = lista[5].classList[1];
+console.log(classe);
 
 while (Contador < 9) {
+    //acessano o elemento da lista
     lista[Contador].onclick = Toca;
+    //contdor mudou de valor
     Contador = Contador + 1;
     console.log(contador);
 }
