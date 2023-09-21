dc61f900b35a808f9958d573907d9d2b9b80b811
AmebaPulante20 <143095752+AmebaPulante20@users.noreply.github.com>
2023-09-21 08:10:20 -0300

    qev

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let contador = 0;
 
 
 
-while (contador < 9) {
+while (contador < lista.length) {
     lista[contador].onclick = Toca;
 const classe = lista[contador].classList[1];
 console.log(classe);
