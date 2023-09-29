5955bce2d41a501eb35f866f185978c12accd991
AmebaPulante20 <143095752+AmebaPulante20@users.noreply.github.com>
2023-09-29 09:36:37 -0300

    kufkt

--- main.js
+++ main.js
@@ -7,10 +7,14 @@ let contador = 0;
 
 while (contador < lista.length) {
     lista[contador].onclick = function () {
-        Toca('#som_tecla_toim');
+        Toca(idAudio);
     }
     const classe = lista[contador].classList[1];
     console.log(classe);
+
+    const idAudio = `#som_${classe}`;
+    console.log(idAudio);
+
     contador = contador + 1;
     console.log(contador);
 
